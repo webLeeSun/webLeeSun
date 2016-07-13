1f5a685b12850def1ea7481d7723cf26403fcd05
李阳 <ly2251585@126.com>
2016-07-13 18:00:39 +0800

    修改BUG

---
+++ xs/js/chat.js
@@ -0,0 +1,133 @@
+//初始化iscroll
+var myScroll;
+
+function loaded() {
+    myScroll = new IScroll('#wrapper', {
+        mouseWheel: true
+    });
+    myScroll.on('scrollStart', function() {
+        $("#text_input").blur();
+        myScroll.refresh();
+    });
+}
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, false);
+
+//input focus 滚动页面 显示输入框
+$("#text_input").focus(function() {
+    setTimeout(function() {
+        $('html,body').animate({
+            scrollTop: $('.chat_footer').offset().top
+        }, 100);
+    }, 200)
+})
+
+$("#text_input").on("input propertychange", function() {
+    if ($.trim($(this).val()) != "") {
+        $(".add").hide();
+        $(".send_message").show();
+    } else {
+        $(".add").show();
+        $(".send_message").hide();
+    }
+})
+
+//管理员右上按钮控制
+$(".control_btn").click(function() {
+    $(".header .cor").toggle();
+    $(".header .control_panel").toggle();
+})
+
+//切换语音文字
+$(".voice").click(function() {
+    $(this).toggleClass("bord");
+    $(".input_box").toggle();
+    $(".voice_on_btn").toggle();
+})
+
+//上传图片
+$("#newImg").on("change", function() {
+    var formData = new FormData($("#myForm")[0]);
+    $.ajax({
+        url: '/upload_image.do',
+        type: 'POST',
+        data: formData,
+        async: false,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(data) {
+          //do something
+        },
+        error: function(e) {
+            alert(e);
+        }
+    });
+})
+
+//websocketJs
+//----连接服务端
+function connect() {
+    // 创建websocket
+    ws = new WebSocket("ws://" + document.domain + ":7272");
+    // 当socket连接打开时，输入用户名
+    ws.onopen = onopen;
+    // 当有消息时根据消息类型显示不同信息
+    ws.onmessage = onmessage;
+    ws.onclose = function() {
+        console.log("连接关闭，定时重连");
+        connect();
+    };
+    ws.onerror = function() {
+        console.log("出现错误");
+    };
+}
+//----连接建立时发送登录信息
+function onopen() {
+    if (!name) {
+        show_prompt();
+    }
+    // 登录
+    var login_data = '{"type":"login","client_name":"' + name.replace(/"/g, '\\"') + '","room_id":"1"}';
+    console.log("websocket握手成功，发送登录数据:" + login_data);
+    ws.send(login_data);
+}
+
+//----服务端发来消息时
+function onmessage(e) {
+    console.log(e.data);
+    var data = eval("(" + e.data + ")");
+    switch (data['type']) {
+        // 发言
+        case 'say':
+            //{"type":"say","from_client_id":xxx,"to_client_id":"all/client_id","content":"xxx","time":"xxx"}
+            say(data['avatar'], data['nickname'], data['content'], data['time']);
+            break;
+    }
+}
+
+//----收到信息
+function say(avatar, nickname, content, time) {
+    $("#chat").append('<div class="msg in">\
+        <div class="avatar">\
+            <img src="' + avatar + '">\
+        </div>\
+        <div class="info">\
+            <h3>' + nickname + '</h3>\
+            <span><b class="cor"></b>' + content + '</span>\
+        </div>\
+    </div>');
+}
+
+//----提交对话
+function onSubmit() {
+    alert("已提交");
+    var input = document.getElementById("text_input");
+    ws.send('{"type":"say","content":"' + input.value);
+    input.value = "";
+}
+
+$("#send_message").click(function() {
+    onSubmit()
+})
---
+++ xs/js/voice.js
@@ -0,0 +1,102 @@
+var localId;
+//需要后台生成签名
+wx.config({
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: '', // 必填，公众号的唯一标识
+    timestamp: "{$sign.timestamp}", // 必填，生成签名的时间戳
+    nonceStr: '{$sign.nonceStr}', // 必填，生成签名的随机串
+    signature: '{$sign.signature}', // 必填，签名，见附录1
+    jsApiList: ["startRecord", 'stopRecord', 'playVoice', 'pauseVoice', 'onVoiceRecordEnd', 'stopVoice', 'uploadVoice', 'downloadVoice'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+});
+wx.ready(function() {
+    // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
+    wx.checkJsApi({
+        jsApiList: [
+            'checkJsApi',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'hideMenuItems',
+            'showMenuItems',
+            'hideAllNonBaseMenuItem',
+            'showAllNonBaseMenuItem',
+            'translateVoice',
+            'startRecord',
+            'stopRecord',
+            'onRecordEnd',
+            'playVoice',
+            'pauseVoice',
+            'stopVoice',
+            'uploadVoice',
+            'downloadVoice',
+            'chooseImage',
+            'previewImage',
+            'uploadImage',
+            'downloadImage',
+            'getNetworkType',
+            'openLocation',
+            'getLocation',
+            'hideOptionMenu',
+            'showOptionMenu',
+            'closeWindow',
+            'scanQRCode',
+            'chooseWXPay',
+            'openProductSpecificView',
+            'addCard',
+            'chooseCard',
+            'openCard'
+        ],
+        success: function(res) {
+            console.log("检测通过：" + JSON.stringify(res));
+        },
+        fail: function(res) {
+            console.log("检测失败：" + JSON.stringify(res));
+        },
+        complete: function(res) {
+            console.log("检测结束");
+        }
+    });
+    //开始录音
+    $('.voice_on_btn').bind("touchstart", function() {
+        $(".mic_pop").toggle();
+        wx.startRecord();
+    });
+    //结束录音-上传
+    $('.voice_on_btn').bind("touchend", function() {
+        $(".mic_pop").toggle();
+        wx.stopRecord({
+            success: function(res) {
+                localId = res.localId;
+                wx.uploadVoice({
+                    localId: localId, // 需要上传的音频的本地ID，由stopRecord接口获得
+                    isShowProgressTips: 1, // 默认为1，显示进度提示
+                    success: function(res) {
+                        var serverId = res.serverId; // 返回音频的服务器端ID
+                        $.ajax({
+                            url: "<php>echo U('Index/downLoadMedia')</php>",
+                            type: "POST",
+                            data: {
+                                media: serverId,
+                                tid: tid,
+                                replyid: replyid
+                            },
+                            success: function(result) {
+                                //do something
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+    });
+
+    wx.onVoiceRecordEnd({
+        // 录音时间超过一分钟没有停止的时候会执行 complete 回调
+        complete: function(res) {
+            localId = res.localId;
+        }
+    });
+
+});
